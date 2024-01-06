fa8501aca638c061d7d33727fb375f499ea14daf
sfoong02 <154398784+sfoong02@users.noreply.github.com>
2024-01-06 22:50:28 +0800

    Update Firebase_ESP32_T1.ino

--- Firebase_ESP32_T1.ino
+++ Firebase_ESP32_T1.ino
@@ -23,22 +23,15 @@
 #define WIFI_PASSWORD ""
 
 // Insert Firebase project API Key
-#define API_KEY ""
-
-// Insert Authorized Email and Corresponding Password
-#define USER_EMAIL ""
-#define USER_PASSWORD ""
+#define API_KEY "AIzaSyCmV_ZIx8mohqcbraP2Y4o_O54b-wMt_uc"
 
 // Insert RTDB URLefine the RTDB URL
-#define DATABASE_URL ""
+#define DATABASE_URL "https://trackie-cfff3-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
 // Define Firebase objects
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
-
-// Variable to save USER UID
-String uid;
 
 // Database main path (to be updated in setup with the user UID)
 String databasePath;
@@ -71,6 +64,7 @@ DallasTemperature sensors(&oneWire);
 // Timer variables (send new readings every three minutes)
 unsigned long sendDataPrevMillis = 0;
 unsigned long timerDelay = 180000;
+bool signupOK = false;
 
 void read_sensor_data(void * parameter) {
    for (;;) {
@@ -129,38 +123,23 @@ void setup(){
   // Assign the api key (required)
   config.api_key = API_KEY;
 
-  // Assign the user sign in credentials
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
-
   // Assign the RTDB URL (required)
   config.database_url = DATABASE_URL;
 
-  Firebase.reconnectWiFi(true);
-  fbdo.setResponseSize(4096);
-
-  // Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
-
-  // Assign the maximum retry of token generation
-  config.max_token_generation_retry = 5;
-
-  // Initialize the library with the Firebase authen and config
-  Firebase.begin(&config, &auth);
-
-  // Getting the user UID might take a few seconds
-  Serial.println("Getting User UID");
-  while ((auth.token.uid) == "") {
-    Serial.print('.');
-    delay(1000);
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
   }
-  // Print user UID
-  uid = auth.token.uid.c_str();
-  Serial.print("User UID: ");
-  Serial.println(uid);
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
 
-  // Update database path
-  databasePath = "/UsersData/" + uid + "/readings";
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 
 // Function to format timestamp to Singapore time
@@ -182,7 +161,7 @@ String formatSingaporeTime(time_t timestamp) {
 void loop(){
 
   // Send new readings to database
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)) {
     sendDataPrevMillis = millis();
 
     //Get current timestamp
